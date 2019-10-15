562410fd12aba4e6cc351c3346d230e19ebd4899
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2019-10-15 10:01:51 +0200

    skipping check on unknown commit and introduced verbose flag

--- main.go
+++ main.go
@@ -11,10 +11,12 @@ import (
 
 var dependenciesStr string
 var fromCommit string
+var verbose bool
 
 func init() {
 	flag.StringVar(&dependenciesStr, "d", "", "directories (comma-separated) to check for change")
 	flag.StringVar(&fromCommit, "c", "", "commit-hash or branch or tag to start checking from")
+	flag.BoolVar(&verbose, "v", false, "increase verbosity for debugging")
 }
 
 func main() {
@@ -24,21 +26,33 @@ func main() {
 	dependencies := strings.Split(dependenciesStr, ",")
 	assertInput(dependencies, fromCommit, command)
 
-	filesChanged, err := detectChangedFiles(fromCommit)
-	if err != nil {
-		log.Fatalf("could not run git-diff to detect changed files: %v\n", err)
+	hash, ok := dereferenceCommit(fromCommit)
+	if verbose {
+		log.Printf("dereferenced %q to %q\n", fromCommit, hash)
+	}
+	if ok {
+		filesChanged, err := detectChangedFiles(hash)
+		if err != nil {
+			log.Fatalf("could not run git-diff to detect changed files: %v\n", err)
+		}
+
+		if !requiresRun(filesChanged, dependencies) {
+			if verbose {
+				log.Println("no change in any dependency, skipping run ...")
+			}
+			return
+		}
+	} else if verbose {
+		log.Printf("could not dereference %q, skipping check ...\n", fromCommit)
 	}
 
-	if !requiresRun(filesChanged, dependencies) {
-		log.Println("no change in any dependency, skipping run ...")
-		return
+	if verbose {
+		log.Printf("running command: %v\n", strings.Join(command, " "))
 	}
-
-	log.Printf("running command: %v\n", strings.Join(command, " "))
 	cmd := exec.Command(command[0], command[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		if exiterr, ok := err.(*exec.ExitError); ok {
 			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
@@ -50,6 +64,16 @@ func main() {
 		}
 	}
 }
+
+func dereferenceCommit(fromCommit string) (string, bool) {
+	out, err := exec.Command("git", "rev-parse", "--verify", fromCommit).Output()
+	if err != nil {
+		return "", false
+	}
+	lines := strings.Split(string(out), "\n")
+	return lines[0], true
+}
+
 func assertInput(deps []string, prevCommit string, command []string) {
 	if deps == nil || len(deps) == 0 {
 		log.Fatal("E: at least 1 dependency (-d) required!")
